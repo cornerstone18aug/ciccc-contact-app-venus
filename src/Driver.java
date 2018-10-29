b0b0bb0f594b47bd89e05e9851eb66a1d8694a49
Yausuhiro Katayama <yasucom0406@gmail.com>
2018-10-29 13:11:30 -0700

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,6 @@
 import controller.InputCollector;
 import model.Contact;
 import model.ContactList;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,7 +9,9 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
 
-        System.out.println("----- MAIN MENU -----");
+        System.out.println("-------------------------------");
+        System.out.println("---------- MAIN MENU ----------");
+        System.out.println("-------------------------------");
         System.out.println(" ");
         System.out.println("new - Create a new contact");
         System.out.println("list - List all contacts");
@@ -27,35 +27,60 @@ public class Driver {
         do {
             // Task2
             if (menuCommand.equals("quit")) {
+                System.out.println(" ");
                 System.out.println("quit - Exit Application");
                 System.exit(0);
             }
 
             // Task3
             if (menuCommand.equals("new")) {
-                System.out.println("Take a full name and email");
-                System.out.println("Enter your name");
+                System.out.println(" ");
+                System.out.printf("Enter your name: ");
                 InputCollector inputCollector = new InputCollector();
                 String userInput = in.nextLine();
                 String user = inputCollector.inputForPrompt(userInput);
 
-                System.out.println("Enter your email");
+                System.out.printf("Enter your email: ");
                 String emailInput = in.nextLine();
                 String email = inputCollector.inputForPrompt(emailInput);
-                Contact contact = new Contact(id, user, email);
-                contactList.addContact(contact);
 
+                System.out.printf("Enter your cellphone: ");
+                String cellphoneInput = in.nextLine();
+                String cellphone = inputCollector.inputForPrompt(cellphoneInput);
+
+                System.out.printf("Enter your address: ");
+                String addressInput = in.nextLine();
+                String address = inputCollector.inputForPrompt(addressInput);
+
+                Contact contact = new Contact(id, user, email, cellphone, address);
+                contactList.addContact(contact);
+                System.out.println(" ");
+                System.out.println("Contact saved as: ");
+                System.out.println(" ");
                 contactList.printList();
+                System.out.println(" ");
+                System.out.println(" ");
+
+
+                id++;
+
 
             }
 
             // Task 4
             if (menuCommand.equals("list")) {
-                System.out.println("list - List all contacts");
+                System.out.println(" ");
+                System.out.println("All contacts");
+                System.out.println(" ");
                 contactList.printList();
             }
 
-            System.out.println("----- MAIN MENU -----");
+
+
+
+            System.out.println("-------------------------------");
+            System.out.println("---------- MAIN MENU ----------");
+            System.out.println("-------------------------------");
             System.out.println(" ");
             System.out.println("new - Create a new contact");
             System.out.println("list - List all contacts");
@@ -63,9 +88,9 @@ public class Driver {
             System.out.println(" ");
             System.out.printf("> ");
 
+
             menuCommand = in.nextLine();
 
-            id++;
 
         } while(!menuCommand.equals("quit"));
 
